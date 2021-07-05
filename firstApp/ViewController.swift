984bc477b474f77936d0807f5eb9d6aaac406fac
Konstantin Prokofiev <flashboyz@yandex.ru>
2021-07-05 23:57:07 +0300

    init commit

---
+++ firstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstApp
+//
+//  Created by Константин Прокофьев on 05.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
